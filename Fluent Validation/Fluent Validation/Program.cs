168ef6c8e861cc67f832379fb9ee427daf8aa1a0
Geronimo Makowiecki <129903462+GeroMako@users.noreply.github.com>
2023-11-06 17:52:14 -0300

    Add files via upload

---
+++ Fluent Validation/Fluent Validation/Program.cs
@@ -0,0 +1,35 @@
+﻿using Fluent_Validation.Entities;
+using Fluent_Validation.Validaciones;
+
+namespace FluentValidation
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            AltaTarjeta A = new AltaTarjeta();
+
+            
+            /* validador de datos personales*/
+            var ValidacionDatospersonales = new ValidacionDatosPersonales();
+            var resultadosPersonales = ValidacionDatospersonales.Validate(A);
+
+            foreach (var error in resultadosPersonales.Errors)
+            {
+                Console.WriteLine(error.PropertyName + ":" + error.ErrorMessage);
+            }
+
+            /* validador de datos pagos*/
+            var ValidacionDatosPago = new ValidacionDatosPago();
+            var resultadosPagos = ValidacionDatosPago.Validate(A);
+
+            foreach (var error in resultadosPagos.Errors)
+            {
+                Console.WriteLine(error.PropertyName + ":" + error.ErrorMessage);
+            }
+
+        }
+
+    }
+
+}
